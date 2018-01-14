da2f7abdedf760fd82debffef70ba4f4fdd6870c
Connor Beck <connor.j.beck@gmail.com>
2018-01-13 17:30:59 -0800

    static service now available

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,10 +1,19 @@
-/**
-* A basic Hello World function
-* @param {string} name Who you're saying hello to
-* @returns {string}
-*/
-module.exports = (name = 'world', context, callback) => {
+const ejs = require('ejs');
+const templatePath = __dirname + '/../templates/index.ejs';
 
-  callback(null, `hello ${name}`);
+/**
+* Renders your Index page from ./templates/index.ejs
+* @returns {buffer}
+*/
+module.exports = (context, callback) => {
+
+  return ejs.renderFile(
+    templatePath,
+    {
+      templateVar: 0
+    },
+    {},
+    (err, response) => callback(err, new Buffer(response || ''), {'Content-Type': 'text/html'})
+  );
 
 };
